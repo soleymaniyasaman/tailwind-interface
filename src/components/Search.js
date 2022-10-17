7ebd9a4ddba415c399ac9d5773cca6e54fa1e20f
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2022-10-17 15:14:10 +0300

    add functionality to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import { BiSearch, BiCaretDown, BiCheck } from 'react-icons/bi'
 
-const DropDown = () => {
+const DropDown = ({ toggle }) => {
+    if (!toggle) {
+        return null;
+    }
     return (
         <div className="origin-top-right absolute right-0 mt-2 w-56
       rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
@@ -25,6 +29,7 @@ const DropDown = () => {
     )
 }
 const Search = () => {
+    const [toggleDropDown, setToggleDropDown] = useState(false);
     return (
         <div className="py-5">
             <div className="mt-1 relative rounded-md shadow-sm">
@@ -36,11 +41,13 @@ const Search = () => {
                     className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
                 <div className="absolute inset-y-0 right-0 flex items-center">
                     <div>
-                        <button type="button"
+                        <button
+                            onClick={() => setToggleDropDown(!toggleDropDown)}
+                            type="button"
                             className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
                             Sort By <BiCaretDown className="ml-2" />
                         </button>
-                        <DropDown />
+                        <DropDown toggle={toggleDropDown} />
                     </div>
                 </div>
             </div>
