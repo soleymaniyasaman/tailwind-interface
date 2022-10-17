f385ef63dbc74a7c452f44f4946d06bc38cb711c
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2022-10-17 17:31:31 +0300

    add searching and sorting function to search box

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ import { useState, useEffect, useCallback } from "react";
 
 function App() {
   const [appointmentList, setAppointmentList] = useState([]);
+  const [query, setQuery] = useState('');
 
+  const [orderBy, setOrderBy] = useState("asc");
+  const [sortBy, setSortBy] = useState("petName");
+
+  const filterAppointment = appointmentList.filter(appointment => {
+    return (
+      appointment.petName.toLowerCase().includes(query.toLowerCase()) ||
+      appointment.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+      appointment.aptNotes.toLowerCase().includes(query.toLowerCase())
+    )
+  }).sort((a, b) => {
+    let order = orderBy === "asc" ? 1 : -1
+    return a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
+  })
   const fetchData = useCallback(() => {
     fetch('./data.json')
       .then(response => response.json())
@@ -24,8 +38,14 @@ function App() {
         Your Appointment
       </h1>
       <AddAppointment />
-      <Search />
-      {appointmentList.map(appointment => (
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)}
+        sortBy={sortBy}
+        onSortByChange={mySort => setSortBy(mySort)}
+        orderBy={orderBy}
+        onOrderByChange={myOrder => setOrderBy(myOrder)}
+      />
+      {filterAppointment.map(appointment => (
         <AppointmentInfo key={appointment.id} appointment={appointment}
           onDeleteAppointment={(appointmentId) => {
             setAppointmentList(appointmentList.filter(appointment => appointment.id !== appointmentId))
